27c9e3a8b8f1869c9306c3a32bbcfa27cd4a9f20
Prakash Rai <raiprakas007@gmail.com>
2017-11-10 15:43:08 -0500

    setup react

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.create({name: "Mary", phone: "8080804382803", email: "mary@gmail.com", address: "Atlanta,GA"})
+User.create({name: "Steve", phone: "80803333", email: "steve@gmail.com", address: "Atlanta,GA"})
+User.create({name: "Bobby", phone: "8080804382803", email: "bobbyrai@gmail.com", address: "Atlanta,GA"})
+
